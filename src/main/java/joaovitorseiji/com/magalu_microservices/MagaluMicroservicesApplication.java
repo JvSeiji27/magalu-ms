91495e330eaafe154050c8fc97eb44ffe1c3dc7d
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-10-15 08:16:58 -0300

    Primeiro commit - configuracao

---
+++ src/main/java/joaovitorseiji/com/magalu_microservices/MagaluMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package joaovitorseiji.com.magalu_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagaluMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joaovitorseiji/com/magalu_microservices/MagaluMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package joaovitorseiji.com.magalu_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
